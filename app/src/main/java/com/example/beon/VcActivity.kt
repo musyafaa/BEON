cd09d197417459260bc8a14385a4ccd5b99f784e
musyafaa <musafadila2132@gmail.com>
2022-04-18 13:08:16 +0700

    revisi lagi

--- app/src/main/java/com/example/beon/VcActivity.kt
+++ app/src/main/java/com/example/beon/VcActivity.kt
@@ -1,11 +1,21 @@
 package com.example.beon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.beon.databinding.ActivityVcBinding
 
 class VcActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityVcBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_vc)
+        binding = ActivityVcBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.decline.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
